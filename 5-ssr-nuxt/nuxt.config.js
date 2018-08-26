fad9a815f827ff2b09dea28ad7b7a3e458f6e64d
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2018-08-26 20:22:18 -0300

    Created the fifth sample

---
+++ 5-ssr-nuxt/nuxt.config.js
@@ -0,0 +1,11 @@
+export default {
+  head: {
+    title: 'Exemplo 5',
+    htmlAttrs: {
+      lang: 'pt-br'
+    },
+    link: [
+      { rel: 'shortcut icon', href: 'favicon.ico' }
+    ]
+  }
+}
---
+++ 5-ssr-nuxt/pages/index.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data () {
+    return {
+      url: this.$route.path,
+      name: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <section id="index">
+    <h2>SRR com Nuxt! 🤑</h2>
+    <p>Você visitou o endereço: <b>{{ url }}</b></p>
+
+    <p>Ir para: <router-link to="/teste">página de teste</router-link></p>
+
+    <input placeholder="Qual seu nome?" v-model="name">
+    <p v-show="name">Olá, {{ name }}!</p>
+  </section>
+</template>
